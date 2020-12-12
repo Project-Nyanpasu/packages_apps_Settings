04216f1f0fa3f1803709704945d17ac2be57ad53
daniml3 <danimoral1001@gmail.com>
2020-12-12 18:40:33 +0100

    PowerUsageSummary: refactor battery temperature unit switch
    * Save whether the user is using Celcius or Fahrenheit on a SharedPreference so it doesn't get erased after exiting the battery settings or after reboot
    * Only change the unit when the user taps the preference, not when its created / the UI is refreshed
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I75851fc2d2ebca2fca6820d4097858a685db3067

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -20,6 +20,8 @@ import static com.android.settings.fuelgauge.BatteryBroadcastReceiver.BatteryUpd
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
@@ -106,7 +108,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     @VisibleForTesting
     BatteryTipPreferenceController mBatteryTipPreferenceController;
 
-    private boolean batteryTemp = false;
+    private SharedPreferences mSharedPreferences;
+    private boolean mUsesCelcius = true;
 
     @VisibleForTesting
     final ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
@@ -222,6 +225,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         super.onCreate(icicle);
         setAnimationAllowed(true);
 
+        mSharedPreferences = getContext().getSharedPreferences("power_usage_summary", 0);
+
         initFeatureProvider();
         mBatteryLayoutPref = (LayoutPreference) findPreference(KEY_BATTERY_HEADER);
 
@@ -236,6 +241,14 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         }
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
+
+        mUsesCelcius = mSharedPreferences.getBoolean("uses_celcius", true);
+        Editor editor = mSharedPreferences.edit();
+        editor.putBoolean("uses_celcius", mUsesCelcius);
+        editor.commit();
+
+        updateBatteryTempPreference(false);
+
     }
 
     @Override
@@ -248,7 +261,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                         .launch();
             return true;
         } else if (KEY_BATTERY_TEMP.equals(preference.getKey())) {
-            updateBatteryTempPreference();
+            updateBatteryTempPreference(true);
         } 
         return super.onPreferenceTreeClick(preference);
     }
@@ -332,7 +345,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         updateLastFullChargePreference();
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
-        updateBatteryTempPreference();
+        updateBatteryTempPreference(false);
     }
 
     @VisibleForTesting
@@ -364,16 +377,18 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     }
 
     @VisibleForTesting
-    void updateBatteryTempPreference() {
-        if (batteryTemp) {
+    void updateBatteryTempPreference(boolean updateTempUnit) {
+        if (updateTempUnit) {
+            mUsesCelcius = !mUsesCelcius;
             mBatteryTemp.setSubtitle(
-                CustomUtils.batteryTemperature(getContext(), false));
-            batteryTemp = false;
+                CustomUtils.batteryTemperature(getContext(), mUsesCelcius));
         } else {
             mBatteryTemp.setSubtitle(
-                CustomUtils.batteryTemperature(getContext(), true));
-            batteryTemp = true;
+                CustomUtils.batteryTemperature(getContext(), mUsesCelcius));
         }
+        Editor editor = mSharedPreferences.edit();
+        editor.putBoolean("uses_celcius", mUsesCelcius);
+        editor.commit();
     }
 
     @VisibleForTesting
