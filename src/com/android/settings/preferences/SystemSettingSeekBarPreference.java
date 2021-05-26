e053a72d48753a31ca1ae22ceb5cf3c78adef401
baolong24 <1667375308@qq.com>
2021-05-26 20:22:02 +0800

    Settings: add missing attrs

--- src/com/android/settings/preferences/SystemSettingSeekBarPreference.java
+++ src/com/android/settings/preferences/SystemSettingSeekBarPreference.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.exthmui.settings.preferences;
+package com.android.settings.preferences;
 
 import android.content.Context;
 import android.util.AttributeSet;
