a0e1eaca77bc7385caab03ad5daeb4135096b72a
ReallySnow <reallysnow233@gmail.com>
2021-07-15 05:30:58 +0800

    Settings: Display Amatsuka version

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -80,10 +80,10 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
     public CharSequence getSummary() {
         StringBuilder sb = new StringBuilder();
         sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
-        String pixelExperienceVersion = VersionUtils.getPixelExperienceVersion();
-        if (!pixelExperienceVersion.equals("")){
+        String amatsukaVersion = VersionUtils.getAmatsukaVersion();
+        if (!amatsukaVersion.equals("")){
             sb.append("\n");
-            sb.append(pixelExperienceVersion);
+            sb.append(amatsukaVersion);
         }
         return sb.toString();
     }
--- src/com/android/settings/deviceinfo/firmwareversion/SimpleBuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/SimpleBuildNumberPreferenceController.java
@@ -39,10 +39,10 @@ public class SimpleBuildNumberPreferenceController extends BasePreferenceControl
     public CharSequence getSummary() {
         StringBuilder sb = new StringBuilder();
         sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
-        String pixelExperienceVersion = VersionUtils.getPixelExperienceVersion();
-        if (!pixelExperienceVersion.equals("")){
+        String amatsukaVersion = VersionUtils.getAmatsukaVersion();
+        if (!amatsukaVersion.equals("")){
             sb.append("\n");
-            sb.append(pixelExperienceVersion);
+            sb.append(amatsukaVersion);
         }
         return sb.toString();
     }
