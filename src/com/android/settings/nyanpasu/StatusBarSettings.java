e74d246dbeb930be2ef3bf0a8161308aa69dfc1c
LibXZR <i@xzr.moe>
2022-01-18 18:58:37 +0800

    Settings: Add clock AM/PM style in Statusbar settings
    Change-Id: I5bd8cb66b9bf54e43c33ffecee9be142217ba215
    Signed-off-by: LibXZR <i@xzr.moe>

--- src/com/android/settings/nyanpasu/StatusBarSettings.java
+++ src/com/android/settings/nyanpasu/StatusBarSettings.java
@@ -7,17 +7,34 @@
 package com.android.settings.nyanpasu;
 
 import android.os.Bundle;
+import android.text.format.DateFormat;
 
 import com.android.internal.logging.nano.MetricsProto; 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import moe.reallysnow.support.preferences.SecureSettingListPreference;
+
 public class StatusBarSettings extends SettingsPreferenceFragment {
+    private static final String KEY_STATUS_BAR_AM_PM = "status_bar_am_pm";
+
+    SecureSettingListPreference mStatusBarAmPm;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.status_bar_settings);
+        mStatusBarAmPm = findPreference(KEY_STATUS_BAR_AM_PM);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (DateFormat.is24HourFormat(requireContext())) {
+            mStatusBarAmPm.setEnabled(false);
+            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_unavailable);
+        }
     }
 
     @Override
