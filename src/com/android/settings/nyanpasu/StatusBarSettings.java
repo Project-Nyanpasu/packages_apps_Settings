8cfa497f5ff11b2d3863c2c56181d288f14cbbe9
ReallySnow <reallysnow233@gmail.com>
2022-10-11 18:37:47 +0800

    Settings: Initial Advanced Settings
    Change-Id: I698e12429ade556000a2118675381d642d4098db

---
+++ src/com/android/settings/nyanpasu/StatusBarSettings.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2022 The Project Nyanpasu
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.settings.nyanpasu;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto; 
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class StatusBarSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.status_bar_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.NYANPASU_SETTINGS;
+    }
+}
